3cf67f588d23e59cc48c59f7d237ebb784fb7f37
Anna Baraeva <pfeffa.hain@gmail.com>
2026-07-30 07:38:16 +0300

    storage update

--- project/server/storage.js
+++ project/server/storage.js
@@ -1,5 +1,37 @@
 const storage = {
-  books: [],
+  books: [
+
+    {
+        "id": "caee1c4e-b1b5-4c96-90d3-97f3583d6746",
+        "title": "Номера моих бывших для спам рассылки",
+        "description": "Распространяется бесплатно",
+        "authors": "Переписаны вручную",
+        "favorite": true,
+        "fileCover": "",
+        "fileName": "",
+        "fileBook": ""
+    },
+    {
+        "id": "1ff5b8d3-088e-4547-917b-1906f3a8661a",
+        "title": "Номера моих бывших для спам рассылки. Том 2",
+        "description": "Распространяется бесплатно",
+        "authors": "Переписаны вручную",
+        "favorite": true,
+        "fileCover": "",
+        "fileName": "",
+        "fileBook": ""
+    },
+    {
+        "id": "6ee70c76-9b27-478b-99cc-282d814e737f",
+        "title": "Одиссея",
+        "description": "",
+        "authors": "",
+        "favorite": "",
+        "fileCover": "",
+        "fileName": "",
+        "fileBook": ""
+    }    
+  ],
 };
 
 
